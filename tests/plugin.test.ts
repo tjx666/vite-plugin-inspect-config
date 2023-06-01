7f16705a8884c81e1280d88248a20077c310bd7f
changfeng <changfeng@gaoding.com>
2023-06-02 03:40:27 +0800

    test: force output with color

--- tests/plugin.test.ts
+++ tests/plugin.test.ts
@@ -12,6 +12,7 @@ const exec = async (command: string) => {
     const result = await execa(cmd, args, {
         cwd: testCwd,
         preferLocal: true,
+        env: { FORCE_COLOR: 'true' },
     });
     return {
         stdout: result.stdout.replaceAll('\r?\n?', '\n'),
@@ -20,5 +21,6 @@ const exec = async (command: string) => {
 
 test('basic test', async () => {
     const result = await exec('vite build -l error -c vite.config.ts');
+    await fs.writeFile(resolve(testCwd, 'stdout.txt'), result.stdout, 'utf8');
     expect(result.stdout).toBe(stdout);
 });
